3339c79a21524b77a3f0b9997f7f2095cec03b18
haha4ni <haha4ni@hotmail.com>
2022-06-05 18:15:03 +0800

    Update Tree.cpp

--- Tree.cpp
+++ Tree.cpp
@@ -8,7 +8,7 @@ struct Tree
 	Tree* right;
 };
 
-void append(Tree** tree, int var)
+void appendNode(Tree** tree, int var)
 {
 	Tree* node = new(Tree);
 	node->var = var;
@@ -27,6 +27,54 @@ void append(Tree** tree, int var)
 	*temp = node;
 }
 
+void deleteNode(Tree** tree, int var)
+{
+	Tree** temp = tree;
+	while(*temp != NULL)
+	{
+		if(var < (*temp)->var)
+			temp = &((*temp)->left);
+		else if(var > (*temp)->var)
+			temp = &((*temp)->right);
+		else
+		{
+			if((*temp)->left == NULL && (*temp)->right == NULL)
+			{
+				delete(*temp);
+				*temp = NULL;
+				return;
+			}
+			else if((*temp)->left == NULL)
+			{
+				Tree* deleteNode = *temp;
+				*temp = (*temp)->right;
+				delete(deleteNode);
+				return;
+			}
+			else if((*temp)->right == NULL)
+			{
+				Tree* deleteNode = *temp;
+				*temp = (*temp)->left;
+				delete(deleteNode);
+				return;
+			}
+			else
+			{
+				Tree** rightMin = &(*temp)->right;
+				while((*rightMin)->left != NULL)
+				{
+					rightMin = &((*rightMin)->left);
+				}
+				(*temp)->var = (*rightMin)->var;
+				delete(*rightMin);
+				*rightMin = NULL;
+				return;
+			}
+		}
+	}
+}
+
+
 void preorder(Tree* tree)
 {
 	cout << tree->var << ",";
@@ -39,18 +87,18 @@ void preorder(Tree* tree)
 void inorder(Tree* tree)
 {
 	if(tree->left != NULL)
-		preorder(tree->left);
+		inorder(tree->left);
 	cout << tree->var << ",";
 	if(tree->right != NULL)
-		preorder(tree->right);
+		inorder(tree->right);
 }
 
 void postorder(Tree* tree)
 {
 	if(tree->left != NULL)
-		preorder(tree->left);
+		postorder(tree->left);
 	if(tree->right != NULL)
-		preorder(tree->right);
+		postorder(tree->right);
 	cout << tree->var << ",";
 }
 
@@ -58,9 +106,13 @@ void postorder(Tree* tree)
 int main() {
 	// your code goes here
 	Tree* tree = NULL;
-	append(&tree, 10);
-	append(&tree, 5);
-	append(&tree, 20);
+	appendNode(&tree, 10);
+	appendNode(&tree, 5);
+	appendNode(&tree, 20);
+	appendNode(&tree, 3);
+	appendNode(&tree, 17);
+	
+	deleteNode(&tree, 10);
 	
 	preorder(tree);
 	cout << endl;
