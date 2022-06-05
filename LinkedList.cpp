9b21abfcf4cac8729456d1830a3a24c878d83c4b
haha4ni <haha4ni@hotmail.com>
2022-06-05 18:12:23 +0800

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,50 +1,124 @@
 #include <iostream>
 using namespace std;
 
-struct LinkedList
+struct Tree
 {
 	int var;
-	LinkedList* next;
+	Tree* left;
+	Tree* right;
 };
 
-void appendNode(LinkedList** list, int var)
+void append(Tree** tree, int var)
 {
-	LinkedList* node = new(LinkedList);
+	Tree* node = new(Tree);
 	node->var = var;
-	node->next = NULL;
+	node->left = NULL;
+	node->right = NULL;
 	
-	LinkedList** temp = list;
+	Tree** temp = tree;
 	while(*temp != NULL)
-		temp = &((*temp)->next);
+	{
+		if(var < (*temp)->var)
+			temp = &((*temp)->left);
+		else
+			temp = &((*temp)->right);
+	}
+	
 	*temp = node;
 }
 
-void deleteNode(LinkedList** list, int var)
+void deleteNode(Tree** tree, int var)
 {
-	LinkedList** temp = list;
-	while(*temp != NULL && (*temp)->var != var)
-		temp = &((*temp)->next);
-	*temp = (*temp)->next;
-}
-
-void printAllNode(LinkedList** list)
-{
-	while(*list != NULL)
+	Tree** temp = tree;
+	while(*temp != NULL)
 	{
-		cout << (*list)->var << ",";
-		*list = (*list)->next;
+		if(var < (*temp)->var)
+			temp = &((*temp)->left);
+		else if(var > (*temp)->var)
+			temp = &((*temp)->right);
+		else
+		{
+			if((*temp)->left == NULL && (*temp)->right == NULL)
+			{
+				delete(*temp);
+				*temp = NULL;
+				return;
+			}
+			else if((*temp)->left == NULL)
+			{
+				Tree* deleteNode = *temp;
+				*temp = (*temp)->right;
+				delete(deleteNode);
+				return;
+			}
+			else if((*temp)->right == NULL)
+			{
+				Tree* deleteNode = *temp;
+				*temp = (*temp)->left;
+				delete(deleteNode);
+				return;
+			}
+			else
+			{
+				Tree** rightMin = &(*temp)->right;
+				while((*rightMin)->left != NULL)
+				{
+					rightMin = &((*rightMin)->left);
+				}
+				(*temp)->var = (*rightMin)->var;
+				delete(*rightMin);
+				*rightMin = NULL;
+				return;
+			}
+		}
 	}
 }
 
-int main() 
-{
-	LinkedList* list = new(LinkedList);
-	
-	appendNode(&list, 100);
-	appendNode(&list, 5);
-	appendNode(&list, 20);
-	deleteNode(&list, 5);
-	printAllNode(&list);
 
+void preorder(Tree* tree)
+{
+	cout << tree->var << ",";
+	if(tree->left != NULL)
+		preorder(tree->left);
+	if(tree->right != NULL)
+		preorder(tree->right);
+}
+
+void inorder(Tree* tree)
+{
+	if(tree->left != NULL)
+		inorder(tree->left);
+	cout << tree->var << ",";
+	if(tree->right != NULL)
+		inorder(tree->right);
+}
+
+void postorder(Tree* tree)
+{
+	if(tree->left != NULL)
+		postorder(tree->left);
+	if(tree->right != NULL)
+		postorder(tree->right);
+	cout << tree->var << ",";
+}
+
+
+int main() {
+	// your code goes here
+	Tree* tree = NULL;
+	append(&tree, 10);
+	append(&tree, 5);
+	append(&tree, 20);
+	append(&tree, 3);
+	append(&tree, 17);
+	
+	deleteNode(&tree, 10);
+	
+	preorder(tree);
+	cout << endl;
+	inorder(tree);
+	cout << endl;
+	postorder(tree);
+	cout << endl;
 	return 0;
 }
